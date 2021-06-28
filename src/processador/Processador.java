494b261a9e3dbd8f57290529330d7e9b2d74b5d2
Matheus Wandermurem <matheusbaldas@hotmail.com>
2021-06-28 12:08:44 -0300

    Método para testar sendo implementado

--- src/processador/Processador.java
+++ src/processador/Processador.java
@@ -7,6 +7,11 @@ import boleto.Boleto;
 public class Processador {
 
 	public static void ProcessaBoletos(ArrayList<Boleto> boletos, Fatura fatura) {
-		
+		if (boletos.get(0).valorPago < fatura.valorTotal) {
+			fatura.pago = false;
+		}
+		else {
+			fatura.pago = true;
+		}
 	}
 }
